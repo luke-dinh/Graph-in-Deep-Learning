e61c9fadeae0dfd8a1b708b9956cd054f3db83a4
luke-dinh <dinhphuoclockt@gmail.com>
2021-11-03 22:09:00 +0700

    Update

--- node_embedding.py
+++ node_embedding.py
@@ -7,4 +7,27 @@ from graph_to_tensor import *
 
 G = nx.karate_club_graph()
 
-emb_sample = nn.Embedding(num_embeddings=4, embedding_dim=8)
+# # Example embedding long tensor
+# emb_sample = nn.Embedding(num_embeddings=4, embedding_dim=8)
+# ids = torch.LongTensor([1, 3])
+# print(ids)
+# print(emb_sample(ids))
+
+# Create node embedding matrix for the graph
+torch.manual_seed(1)
+
+def create_node_emb(num_node=34, embedding_dim=16):
+
+    emb = nn.Embedding(num_embeddings=num_node, embedding_dim=embedding_dim)
+    emb.weight.data = torch.randn(num_node, embedding_dim)
+
+    return emb 
+
+emb = create_node_emb()
+ids = torch.LongTensor([0, 3])
+
+# Print the embedding layer
+print("Embedding: {}".format(emb))
+
+# Gets the embedding from node 0 and 3
+print(emb(ids))
