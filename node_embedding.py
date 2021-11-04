b996997793d3ed0483f7bc2ae26216b023b3f926
luke-dinh <dinhphuoclockt@gmail.com>
2021-11-04 15:00:20 +0700

    Update

--- node_embedding.py
+++ node_embedding.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import matplotlib.pyplot as plt
 from sklearn.decomposition import PCA
+from torch.nn.modules.activation import Sigmoid
+from torch.optim import SGD
 
 from graph_to_tensor import *
 
@@ -33,27 +35,76 @@ print("Embedding: {}".format(emb))
 # Gets the embedding from node 0 and 3
 print(emb(ids))
 
-# Visualize node embeddings
-def visualize_emb(emb):
-  X = emb.weight.data.numpy()
-  pca = PCA(n_components=2)
-  components = pca.fit_transform(X)
-  plt.figure(figsize=(6, 6))
-  club1_x = []
-  club1_y = []
-  club2_x = []
-  club2_y = []
-  for node in G.nodes(data=True):
-    if node[1]['club'] == 'Mr. Hi':
-      club1_x.append(components[node[0]][0])
-      club1_y.append(components[node[0]][1])
-    else:
-      club2_x.append(components[node[0]][0])
-      club2_y.append(components[node[0]][1])
-  plt.scatter(club1_x, club1_y, color="red", label="Mr. Hi")
-  plt.scatter(club2_x, club2_y, color="blue", label="Officer")
-  plt.legend()
-  plt.show()
+# # Visualize node embeddings
+# def visualize_emb(emb):
+#   X = emb.weight.data.numpy()
+#   pca = PCA(n_components=2)
+#   components = pca.fit_transform(X)
+#   plt.figure(figsize=(6, 6))
+#   club1_x = []
+#   club1_y = []
+#   club2_x = []
+#   club2_y = []
+#   for node in G.nodes(data=True):
+#     if node[1]['club'] == 'Mr. Hi':
+#       club1_x.append(components[node[0]][0])
+#       club1_y.append(components[node[0]][1])
+#     else:
+#       club2_x.append(components[node[0]][0])
+#       club2_y.append(components[node[0]][1])
+#   plt.scatter(club1_x, club1_y, color="red", label="Mr. Hi")
+#   plt.scatter(club2_x, club2_y, color="blue", label="Officer")
+#   plt.legend()
+#   plt.show()
 
-# Visualize the initial random embeddding
-visualize_emb(emb)
+# # Visualize the initial random embeddding
+# visualize_emb(emb)
+
+# Training the Embedding
+
+def accuracy(pred, label):
+
+  accu = 0.0
+
+  pred = [1 if item>0.5 else 0 for item in pred]
+  matching = (np.array(pred) == np.array(label)).sum()
+  accu += round(matching/len(label), 4)
+
+  return accu 
+
+def train(emb, loss_fn, sigmoid, train_label, train_edge):
+
+  epochs = 100
+  learning_rate = 0.1
+
+  optimizer = SGD(emb.parameters(), lr=learning_rate, momentum=0.9)
+
+  for i in range(epochs):
+
+    # Get the embeddings of the nodes in train_edge
+    emb_u = emb(train_edge)[0]
+    emb_v = emb(train_edge)[1]
+
+    # Sum of dot product the embeddings of the nodes in train_edge
+    dot_product = torch.sum(np.dot(emb_u, emb_v), dim=-1)
+
+    # Feed the dot product into sigmoid function
+    sig = sigmoid(dot_product)
+
+    # Feed the sigmoid output into loss_fn
+    loss = loss_fn(sig, train_label)
+
+    # Print both loss and acc of each epoch
+
+    print("Loss for epoch {i} : {}".format(loss))
+    print("Accuracy for epoch {i} is: {}".format(accuracy(sig, train_label)))
+
+    # Update the embeddings
+
+    loss.backward() # Backprop
+    optimizer.step() # Update parameter
+
+loss_fn = nn.BCELoss()
+sigmoid = nn.Sigmoid()
+
+print()
