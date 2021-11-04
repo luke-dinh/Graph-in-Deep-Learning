c0346896f2e7bf49f9a84e793a659e2c56e1d4d4
luke-dinh <dinhphuoclockt@gmail.com>
2021-11-04 22:56:29 +0700

    Update

--- utils.py
+++ utils.py
@@ -1,7 +1,9 @@
 import numpy as np
-import torch
 import random
+
+import torch 
 import torch.nn as nn
+from torch.optim import SGD
 
 # Graph edge list and convert to tensor
 def graph_to_edge_list(G):
@@ -53,3 +55,48 @@ def create_node_emb(num_node=34, embedding_dim=16):
     emb.weight.data = torch.randn(num_node, embedding_dim)
 
     return emb 
+
+# Training the Embedding
+class train_node_emb():
+
+    def accuracy(self, pred, label):
+
+        accu = 0.0
+
+        pred = [1 if item>0.5 else 0 for item in pred]
+        matching = (np.array(pred) == np.array(label)).sum()
+        accu += round(matching/len(label), 4)
+
+        return accu 
+
+    def train(self, emb, loss_fn, sigmoid, train_label, train_edge):
+
+        epochs = 500
+        learning_rate = 0.1
+
+        optimizer = SGD(emb.parameters(), lr=learning_rate, momentum=0.9)
+
+        for i in range(epochs):
+
+            # Get the embeddings of the nodes in train_edge
+            emb_u = emb(train_edge)[0]
+            emb_v = emb(train_edge)[1]
+
+            # Sum of dot product the embeddings of the nodes in train_edge
+            dot_product = torch.sum(emb_u * emb_v, dim=-1)
+
+            # Feed the dot product into sigmoid function
+            sig = sigmoid(dot_product)
+
+            # Feed the sigmoid output into loss_fn
+            loss = loss_fn(sig, train_label)
+
+            # Update the embeddings
+
+            loss.backward() # Backprop
+            optimizer.step() # Update parameter
+
+            # Print both loss and acc of each epoch
+
+            print("Loss for epoch {} : ".format(i), loss)
+            print("Accuracy for epoch {} is: ".format(i), self.accuracy(sig, train_label))
