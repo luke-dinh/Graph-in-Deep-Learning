d8a0c147a01a6e3670094dc904b6e7b7d3ae4430
luke-dinh <dinhphuoclockt@gmail.com>
2021-11-08 17:17:33 +0700

    Update

--- Example2/GNN.py
+++ Example2/GNN.py
@@ -3,8 +3,9 @@ import os
 import torch
 import pandas as pd
 import torch.nn.functional as F
-import torch_geometric.transforms as T
+import torch.nn as nn
 
+import torch_geometric.transforms as T
 from torch_geometric.nn import GCNConv
 from ogb.nodeproppred import PygNodePropPredDataset, Evaluator
 
@@ -27,3 +28,35 @@ if 'IS_GRADESCOPE_ENV' not in os.environ:
     data = data.to(device)
     split_idx = dataset.get_idx_split()
     train_idx = split_idx['train'].to(device)
+
+class GCN(torch.nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim, num_layers, 
+                    dropout, return_embeds = False):
+
+        super(GCN, self).__init__()
+
+        # GCN_Convs layers
+        self.convs = nn.ModuleList([GCNConv(input_dim, hidden_dim)])
+        self.convs.extend(nn.ModuleList([GCNConv(hidden_dim, hidden_dim) for i in range(num_layers-2)]))
+        self.convs.extend(nn.ModuleList([GCNConv(hidden_dim, output_dim)]))
+
+        # Batchnorm layers
+        self.batch_norm = nn.ModuleList([nn.BatchNorm1d(hidden_dim) for i in range(num_layers-1)])
+
+        # Log Softmax layers
+        self.softmax = nn.LogSoftmax(dim=1)
+
+        # Probability of an element getting zeroed
+        self.dropout = dropout
+
+        # Skip classification layer and return node embeddings
+        self.return_embeds = return_embeds
+
+    def reset_parameters(self):
+        for conv in self.convs:
+            conv.reset_parameters()
+        for bn in self.batch_norm:
+            bn.reset_parameters()
+
+    # Feed forward
+
