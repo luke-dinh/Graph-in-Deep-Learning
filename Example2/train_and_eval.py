d179ee53e75ba5105be99f63bfd470b6b701bf60
luke-dinh <dinhphuoclockt@gmail.com>
2021-11-09 21:13:28 +0700

    Update

--- Example2/train_and_eval.py
+++ Example2/train_and_eval.py
@@ -1,15 +1,33 @@
 import sys
 sys.path.append('Example2')
 
+from ogb.nodeproppred import PygNodePropPredDataset, Evaluator
 from Example2.GNN import *
 import copy
 import os 
 
 if 'IS_GRADESCOPE_ENV' not in os.environ:
 
+    # Load the dataset
+    dataset_name = 'ogbn-arxiv'
+    dataset = PygNodePropPredDataset(name=dataset_name,
+                                  transform=T.ToSparseTensor())
+    data = dataset[0]
+
+    # Make the adjacency matrix to symmetric
+    data.adj_t = data.adj_t.to_symmetric()
+
+    # Only cpu is valilable
+    device = 'cpu'
+
+    # Load the data to device
+    data = data.to(device)
+    split_idx = dataset.get_idx_split()
+    train_idx = split_idx['train'].to(device)
+
     # Define arguments
     args = { 
-        'device': 'cpu',
+        'device': device,
         'num_layers': 3,
         'hidden_dim': 256,
         'dropout': 0.5,
