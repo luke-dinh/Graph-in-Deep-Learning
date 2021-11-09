1ad3967e039a64256be921c7597046a1aec14196
luke-dinh <dinhphuoclockt@gmail.com>
2021-11-09 21:19:40 +0700

    Update

--- Example2/train_and_eval.py
+++ Example2/train_and_eval.py
@@ -47,3 +47,25 @@ if 'IS_GRADESCOPE_ENV' not in os.environ:
 
     # Load the evaluator
     evaluator = Evaluator(name='ogbn-arxiv')
+
+      # reset the parameters to initial random value
+    model.reset_parameters()
+
+    optimizer = torch.optim.Adam(model.parameters(), lr=args['lr'])
+    loss_fn = F.nll_loss
+
+    best_model = None
+    best_valid_acc = 0
+
+    for epoch in range(1, 1 + args["epochs"]):
+        loss = train(model, data, train_idx, optimizer, loss_fn)
+        result = test(model, data, split_idx, evaluator)
+        train_acc, valid_acc, test_acc = result
+        if valid_acc > best_valid_acc:
+            best_valid_acc = valid_acc
+            best_model = copy.deepcopy(model)
+        print(f'Epoch: {epoch:02d}, '
+              f'Loss: {loss:.4f}, '
+              f'Train: {100 * train_acc:.2f}%, '
+              f'Valid: {100 * valid_acc:.2f}% '
+              f'Test: {100 * test_acc:.2f}%')
