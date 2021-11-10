8c474233dc36183a0e9f1c3b033b4eccce0b2ee8
luke-dinh <dinhphuoclockt@gmail.com>
2021-11-10 21:35:11 +0700

    Update

--- Example2/GNN_Graph_Clustering.py
+++ Example2/GNN_Graph_Clustering.py
@@ -73,3 +73,41 @@ def train(model, device, data_loader, optimizer, loss_fn):
             optimizer.step()
 
         return loss.item()
+
+# The evaluation function
+def eval(model, device, loader, evaluator, save_model_results=False, save_file=None):
+    model.eval()
+    y_true = []
+    y_pred = []
+
+    for step, batch in enumerate(tqdm(loader, desc="Iteration")):
+        batch = batch.to(device)
+
+        if batch.x.shape[0] == 1:
+            pass
+        else:
+            with torch.no_grad():
+                pred = model(batch)
+
+            y_true.append(batch.y.view(pred.shape).detach().cpu())
+            y_pred.append(pred.detach().cpu())
+
+    y_true = torch.cat(y_true, dim = 0).numpy()
+    y_pred = torch.cat(y_pred, dim = 0).numpy()
+
+    input_dict = {"y_true": y_true, "y_pred": y_pred}
+
+    if save_model_results:
+        print ("Saving Model Predictions")
+        
+        # Create a pandas dataframe with a two columns
+        # y_pred | y_true
+        data = {}
+        data['y_pred'] = y_pred.reshape(-1)
+        data['y_true'] = y_true.reshape(-1)
+
+        df = pd.DataFrame(data=data)
+        # Save to csv
+        df.to_csv('ogbg-molhiv_graph_' + save_file + '.csv', sep=',', index=False)
+
+    return evaluator.eval(input_dict)
