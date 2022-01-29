81209f151c6c352878411429e05cfbac5d35215c
luke-dinh <dinhphuoclockt@gmail.com>
2022-01-30 03:45:25 +0700

    Update

--- Example3/gnn_stack_module.py
+++ Example3/gnn_stack_module.py
@@ -15,3 +15,25 @@ from torch_sparse import SparseTensor, set_diag
 from torch_geometric.nn.conv import MessagePassing
 from torch_geometric.utils import remove_self_loops, add_self_loops, softmax
 
+
+class GNN_stack(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim, output_dim, args, emb=False):
+
+        super(GNN_stack, self).__init__()
+        conv_model = self.build_conv_model(args.model_type)
+        self.convs = nn.ModuleList()
+        self.convs.append(conv_model(input_dim, hidden_dim))
+
+        assert (args.num_layers >= 1), 'Number of layers is not >=1'
+
+        for l in range(args.num_layers -1 ):
+            self.convs.append(conv_model(args.heads * hidden_dim, hidden_dim))
+
+        # post-message-passing
+        self.post_mp = nn.Sequential( 
+            nn.Linear(args.heads * hidden_dim, hidden_dim), nn.Dropout(args.dropout),
+            nn.Linear(hidden_dim, output_dim)
+        )
+
+        
